065dd3d8cd780ab87e28412fbfefb1c9ed764463
isaacbarcroft <i_barcroft@yahoo.com>
2021-09-26 21:12:21 -0400

    edit/put

--- main.js
+++ main.js
@@ -7,12 +7,15 @@
     const delButton = document.querySelector('.delete')
     const inputButton = document.querySelector('.message').value
     const senderButton = document.querySelector('.sender').value
+    const editButton = document.querySelector('.edit')
     
 
     function generateHTML(message) {
         // construct the message html
         return `
+        <br>
             <li>
+                <p>${message.id}</p>
                 <p>${message.text}</p>
                 <span>${message.username}</span>
             </li>
@@ -36,7 +39,7 @@
             });
     }
 
-    fetchMessages();
+    // fetchMessages;
     setInterval(fetchMessages, 3000);
 
     
@@ -64,7 +67,16 @@
     }
     delButton.addEventListener('click', messageClear);
 
-    chatForm.addEventListener('submit',addMessage);
+    chatForm.addEventListener('submit', function(){
+        console.log('hit', editButton.value);
+        if (editButton.value) {
+            console.log('hit', editButton.value)
+            editText(editButton.value);
+        } else {
+        addMessage();
+    }
+    });
+
         
    
 function messageClear() {
@@ -86,72 +98,34 @@ function messageClear() {
            })
         }
     }
+function editText(id){
+    event.preventDefault();
+const message = {
+    id: id,
+    text: event.target.message.value,
+    username: event.target.sender.value,
+}
+console.log(message);
+    fetch(`https://tiny-taco-server.herokuapp.com/Chatty'/${id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type':  'application/json',
+        }, 
+        body: JSON.stringify(message),
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        html = generateHTML(data);
+            messageList.insertAdjacentHTML('beforeend', html);
+            chatForm.reset();
+    })
 
-//     $submit.addEventListener('click', submitMessage())
-
-    
-//     function submitMessage(){
-//         let chatSent = message.value
-//         let createMessages = {
-//             messages: chatSent,
-//             sender:  user,
-//     }
-//     console.log(chatSent)
-
-
-// message.value= ""
-// renderText(createMessages.messages)
-
-//  }
-    
-//     fetch('https://tiny-taco-server.herokuapp.com/Chatty/')
-//     .then(response => response.json())
-//     .then(function(message){
-//         renderText(message)
-//         console.log(message)
-//     })
-
-//     function renderText(messages){
-//             let length = messages.length;
-//             console.log(messages);
-//             let mostRecent = messages.slice(length -50);
-//             const list = document.querySelector("#message-list");
-//             let newList = ''
-//             console.log(sender.value)
-//             mostRecent.forEach(message => {
-//                 if (!document.querySelector(`li[data-id ='${message.id}']`)){
-//                     newList += makeLi(message)
-//                     }
-//                 })
-//                 if (newList != ""){
-//                     list.innerHTML += newList
-//                     }
-//                 }
-//  function makeLi(messages){
-//      let sender = document.querySelector('#sender').value
-//      if (messages.sender == sender){
-//          sender = "You"
-//      } else {
-//          sender = messages.sender
-//      }
-//      return `
-//    <li data-id = '${message.id}' ><strong>${sender}:<strong> ${message.message}</li>
-//    `
-
-//  }
-
-//  form.addEventListener('submit', postMessage(createMessages))
-
-
-// fetch('https://tiny-taco-server.herokuapp.com/Chatty/137.', {
-//     method: 'DELETE',
-
-// })
-// .then(response => response.json());
-// .then (response => {
-//     if(!response.ok){
-//         throw new ERROR ('OOPS')
-//     }
-//     console.log("DELTED")
-//     return response.json();
-// 
+    .then (response => {
+        if(!response.ok){
+            throw new ERROR ('OOPS')
+        }
+        console.log("DELTED")
+        return response.json();
+    })
+}
